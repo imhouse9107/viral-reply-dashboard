128f7ad233c0f7c874544eac11a6bdb843f3c8f7
Henry WIlliams <henrywilliams@mac.home>
2026-03-21 18:20:58 +0000

    Update dashboards 2026-03-21

---
+++ docs/sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'vrb-v1';
+
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((names) =>
+      Promise.all(names.filter((n) => n !== CACHE_NAME).map((n) => caches.delete(n)))
+    )
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  // Network-first strategy: always try fresh content, fall back to cache
+  e.respondWith(
+    fetch(e.request)
+      .then((response) => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(e.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
